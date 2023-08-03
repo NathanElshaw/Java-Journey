02a77845f6171b60c9283bff80ef548a17eb3352
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-02 23:05:24 -0400

    Swing tinkering

--- org/src/App.java
+++ org/src/App.java
@@ -1,57 +1,30 @@
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JLabel;
+import java.awt.Dimension;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
 
-public class App implements ActionListener {
-
-    private int count = 0;
-    private JLabel label;
-    private JFrame frame;
+public class App {
     private JPanel panel;
-    private JButton button;
-    private JTextField textField;
+    private JFrame frame;
+    private JLabel label;
 
     public App() {
-        frame = new JFrame();
         panel = new JPanel();
-
-        button = new JButton("Login");
-        button.addActionListener(this);
+        frame = new JFrame();
         label = new JLabel();
-        textField = new JTextField(20);
 
-        panel.setBorder(BorderFactory.createEmptyBorder(100, 175, 100, 175));
-        frame.setSize(350, 200);
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("gui");
-        frame.pack();
-
+        frame.setTitle("New Gui");
         label.setBounds(10, 25, 80, 25);
-        label.setText("Username:");
-        textField.setBounds(100, 20, 165, 25);
-        panel.add(label);
-        panel.add(textField);
+        label.setText("New Text");
+        panel.setBorder(BorderFactory.createEmptyBorder(100, 175, 100, 175));
+        frame.setMinimumSize(new Dimension(100, 100));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(label);
+        frame.pack();
 
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
         new App();
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("Number of Clicks: " + count);
-        ;
     }
 }
