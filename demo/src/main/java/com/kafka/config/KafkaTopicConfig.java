84730e375b9cf5df2d6b683f8b34f50c6835d2d4
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-21 12:02:13 -0400

    Added: kafka topic config

--- demo/src/main/java/com/kafka/config/KafkaTopicConfig.java
+++ demo/src/main/java/com/kafka/config/KafkaTopicConfig.java
@@ -1,16 +1,21 @@
 package com.kafka.config;
 
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Configuration
+@ConfigurationProperties(prefix = "planos.kafka")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class KafkaTopicConfig {
 
-    @Bean
-    public NewTopic kafkaTopic() {
-        return TopicBuilder.name("Planos")
-                .build();
-    }
+    private String topic;
+
 }
