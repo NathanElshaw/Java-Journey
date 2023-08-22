407465d7559e1fc60d3bd16c17d6143c65d059fd
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-21 22:43:36 -0400

    Added: consumer

--- demo/src/main/java/com/kafka/DemoApplication.java
+++ demo/src/main/java/com/kafka/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -38,5 +39,11 @@ public class DemoApplication {
 		return args -> {
 			kafkaTemplate.send(KafkaTopicConfig.getTopic(), payload);
 		};
+	}
+
+	@KafkaListener(topics = "customer.visit")
+	public String listens(final String in) {
+		System.out.println(in);
+		return in;
 	}
 }
