1be980c4a61f81bb6503bef280ed071718d1bddc
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-23 22:55:08 -0400

    Added: Learning basics

---
+++ Fundamentals/Syntax.java
@@ -0,0 +1,29 @@
+package Fundamentals;
+/*
+ Class-
+    is a blue print of the instance of a class that
+    can share common properties and methods
+
+ Object-
+    The object is an instance of a class. it has an entity that has 
+    behavior and state
+
+ Method-
+    The behavior of an object is the method
+
+ Instance variable-
+    The state of an object is generally created by the 
+    values that are assigned to these instance variables 
+ */
+
+import java.util.*;
+
+public class Syntax {
+   public static void main(String[] args) {
+      System.out.println("Output");
+   }
+}
+
+/*
+ 
+ */
