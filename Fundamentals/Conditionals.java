f5ead9065cdbe13f8fd9c604323fcac90c13bf1a
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-24 22:06:04 -0400

    Added: notes on conditionals

---
+++ Fundamentals/Conditionals.java
@@ -0,0 +1,48 @@
+package Fundamentals;
+
+public class Conditionals {
+
+}
+
+/*
+ * Conditionals
+ * they are prestated conditions of true / false whre if the condition is true
+ * the code in the block will be ran
+ * 
+ * all programming languages have them
+ * 
+ * if, else if and else are conditional statements in all langauages
+ * c, and C++ have if else
+ * python has elif
+ * C# java, javascript and python have switch case
+ * 
+ * if statement:
+ * takes a condition as a parameter and if it is true then runs the code
+ * if(condition){
+ * code to run
+ * }
+ * 
+ * else if or elif statment:
+ * similar to a if statment used to chain if statments mainly
+ * else if(condition){
+ * code to run
+ * }
+ * 
+ * else statment:
+ * this is ran if all the if statments run and none of the conditionals return
+ * true
+ * else{
+ * code to run
+ * }
+ * 
+ * switch statement:
+ * if, else if, and else combined into one form for a switch takes a condition
+ * as a parameter and cases can contain possible cases of that condition
+ * switch(condition){
+ * case(condition of condition input):code to run
+ * }
+ * 
+ * Tenerary operator:
+ * like a if else statement but written differently
+ * Condition ? if true : if false
+ */
