025b031f410b7344eb47a286725ed3899c338ac2
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-26 22:03:36 -0400

    Added: notes on functions

---
+++ Fundamentals/Functions.java
@@ -0,0 +1,14 @@
+package Fundamentals;
+
+public class Functions {
+
+}
+
+/*
+ * Method Declaration:
+ * Public(Access Specifier) int(Return type) name(Method Name) (Parameters) >
+ * Method header
+ * 
+ * Access Specifier:
+ * This specifies the acess type of the method and the visiability of it
+ */
