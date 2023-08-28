e185970c9f2731ed3d40e83c357d636628a9c4db
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-27 20:11:55 -0400

    Added: more methods notes

--- Fundamentals/Functions.java
+++ Fundamentals/Functions.java
@@ -10,5 +10,46 @@ public class Functions {
  * Method header
  * 
  * Access Specifier:
- * This specifies the acess type of the method and the visiability of it
+ * This specifies the acess type of the method and the visiability of it.
+ * these include:
+ * Public: Access from all classes
+ * Private: Access from classes that specify it
+ * Protected: Access from the same package or subclass
+ * Default: Visible from only the same package
+ * 
+ * Return type:
+ * the return data type if it doesnt return anything then its void
+ * 
+ * Method Name:
+ * the name of the method usually linked to the functionality of the method like
+ * Add() or Subtract()
+ * 
+ * Parameter list:
+ * each parameter is seperated by a comma and is used with in the function
+ * 
+ * Method Body:
+ * this contains all the actions with in the method enclosed with curly braces
+ */
+
+/*
+ * Naming a Method:
+ * should be a verb and start with a lower case letter. usally followed by a
+ * noun or adj to better describe the methods use
+ * 
+ * Single word methods:
+ * named as sum() or area()
+ * 
+ * Muli word methods:
+ * named as areaOfCricle() or stringCompression()
+ * 
+ * its possible to have the same name in a method as another in the same class
+ * its know as method overloading
+ */
+
+/*
+ * Types of Methods:
+ * Pre-defined and User defiend:
+ * 
+ * Java comes with some predefined methods such as length(), toString() and
+ * equals()
  */
