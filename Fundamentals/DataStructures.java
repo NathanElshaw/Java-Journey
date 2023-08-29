ec908b5f61debac688d83bcd06458daff0fb2eb4
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-28 22:31:37 -0400

    Notes of data structures

---
+++ Fundamentals/DataStructures.java
@@ -0,0 +1,37 @@
+package Fundamentals;
+
+public class DataStructures {
+
+}
+
+/*
+ * Data Sctructures:
+ * They are a way of storing, processing, retrieving and storing data. there are
+ * different basic and advanced types of data structures and are used in amlost
+ * every program
+ * 
+ * Classification of data structures:
+ * Linear data structure:
+ * Data that is arranged in sequentally or linearly, where each element is
+ * attached to it previous and next adjacent elements
+ * 
+ * Static data sctructure:
+ * Static data structure has a fixed memory, its easier access the elements in a
+ * static data structure.
+ * 
+ * Ex: Arrays
+ * 
+ * Dynamic data Structure:
+ * in a dynamic data structure the size is not fixed. ut can be randomly updated
+ * during runtime which may be considered effcient concering the memory (space)
+ * complexity of the code
+ * 
+ * Ex: queue, stacks
+ * 
+ * Non-Linear data structures:
+ * Data structures that have data elements that are not places sequentially or
+ * linearly are called non-linear data structures, and cannot be traversed in a
+ * single run only
+ * 
+ * Ex: Trees and Graphs
+ */
