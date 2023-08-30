64cb79e9c1fc32ca5475b313b1cd7879d9d202f3
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-29 22:34:40 -0400

    Added: linked list notes

---
+++ Fundamentals/Data_Structures/LinkedList.java
@@ -0,0 +1,55 @@
+package Fundamentals.Data_Structures;
+
+public class LinkedList {
+}
+
+/*
+ * Linked Lists:
+ * Linked list are linear data structures that include a series of connected
+ * nodes. Linked list can be defined as the nodes that are randomly stored in
+ * memory .
+ * A node contains two parts, first the data part and second is the address
+ * part.
+ * the last node in a list contains a pointer to the null. After arrays LL is
+ * the second most used data structure.
+ * In a linked list, every link contains a connection to another link.
+ */
+
+/*
+ * Visual
+ * (Head) [1200] ->
+ * [[8(Data)][1300(Link)]]->[[12(Data)][1400(Link)]]->[[124][Null(Tail)]
+ */
+
+/*
+ * Why use Linked List:
+ * Linked list overcome the limitations of arrays. Here are some examples
+ * 
+ * The size of the array must be known in advanced before using it in a program.
+ * 
+ * Increasing the size of an array is a time taking process. it is almost
+ * impossible to expand the size of the array at run time
+ * 
+ * All the elements in the array need to be contiguosly stored in memory.
+ * inserting an element in the array needs shifting of all its predecessors
+ * 
+ * Its useful because-
+ * it allocates the memory dyanmically. all the nodes of a linked list are
+ * non-contiguously stored in the memory and linked together with the help of
+ * pointers
+ * 
+ * in Linked list, size is no longer a problem since we do not need to define
+ * its size at the time of declaration. Lists grow as per the programs demand
+ * and limited to available memory space.
+ */
+
+/*
+ * How to declare a linked list:
+ * it simple to declare an array, as it is of single type, while declaration of
+ * a linked list is a bit more typical than array.
+ * 
+ * Linked list contain two parts and both are different types, one is a data
+ * value and another is a pointer variable.
+ * 
+ * 
+ */
