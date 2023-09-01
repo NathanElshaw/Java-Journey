a83e7e21f8b5600b785d2c205301872142cd4721
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-31 23:46:54 -0400

    Added: notes on stacks

--- Fundamentals/Data_Structures/Stacks.java
+++ Fundamentals/Data_Structures/Stacks.java
@@ -5,5 +5,22 @@ public class Stacks {
 }
 
 /*
+ * What is a stack?
+ * A stack is a linear data structure that follows LIFO(Last in first out)
+ * principle.
+ * Stack has one end, where as the Queue has two ends(front and rear). it
+ * contains omly one pointer,top pointer, pointing to the top most element of
+ * the stack.
+ * in other words a stack can be defined as a container in which insertion and
+ * deletion can be done from the one end known as the top of the stack.
  * 
+ * Key Points:
+ * its called a stack because it behaves like a real-world stack, piles of books
+ * things like that.
+ * 
+ * A stack is a abstract data type with a pre defined capcity which means that
+ * it can store the elements of a limited size.
+ * 
+ * it is a data structure that follows some order to insert and delete the
+ * elements, and that order can be LIFO or FILO.
  */
