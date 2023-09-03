0d7b25ba0b922c01d9af8950c19a77e9a12ac9cc
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-09-02 22:56:14 -0600

    Added: queue notes

---
+++ Fundamentals/Data_Structures/Queue.java
@@ -0,0 +1,38 @@
+package Fundamentals.Data_Structures;
+
+public class Queue {
+
+}
+
+/*
+ * Queue:
+ * 
+ * What is a queue:
+ * a Queue can be defined as an ordered list which enables insert opertation to
+ * be performed at one end called REAR and delete operations to be performed at
+ * another end called FRONT
+ * 
+ * A queue is reffered to be as First in First out list.
+ * 
+ * For example people waiting in a line is a queue
+ * 
+ * Applications of Queue
+ * Due to the fact that queue performs actions on a first in first out basis
+ * which is quite fair for the ordering actions. There are various applications
+ * of queues.
+ * 
+ * Queues are wildy used as waiting lists for a single shared resource like
+ * printer, disk, or cpu.
+ * 
+ * Queues are used in asynchrounous transfer of data (Where data is not being
+ * transferred at the same rate between two process) like pipes, file io, or
+ * sockets
+ * 
+ * Queues are used as buffers in most of the applications like mp3 player, cd
+ * player
+ * 
+ * Queues are used to main tain the playlist in media players inorder to add or
+ * remove songs from the list
+ * 
+ * Queues are used in operating systems to handle interrupts.
+ */
