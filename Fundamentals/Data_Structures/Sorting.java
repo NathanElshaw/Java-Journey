6601697719e0d86996d3924fc477a16d57cd81da
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-09-03 20:35:34 -0400

    Added: notes on sorting

---
+++ Fundamentals/Data_Structures/Sorting.java
@@ -0,0 +1,13 @@
+package Fundamentals.Data_Structures;
+
+public class Sorting {
+
+}
+
+/*
+ * Bubble sorting:
+ * Bubble sorting works on the repeatedly swapping of adjacent elements until
+ * are not in the intended order. is called bubble sorting because the movemnt
+ * of array elements is just like the movement of air bubbles in the water.
+ * Bubble in the water rise up to the surface
+ */
