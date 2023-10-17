f935aa7a36806991b59292eb36c521776487f739
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-16 21:04:20 -0400

    Removed: api endpoint

--- Spring_Boot_Learning/src/main/java/com/example/spring_boot_learning/SpringBootLearningApplication.java
+++ Spring_Boot_Learning/src/main/java/com/example/spring_boot_learning/SpringBootLearningApplication.java
@@ -1,21 +1,14 @@
 package com.example.spring_boot_learning;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringBootLearningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLearningApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String hello() {
-		return "Hello";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearningApplication.class, args);
+    }
 
 }
