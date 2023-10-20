29b9f4a323d922748461b365d28c9f05e7ae17b7
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-19 23:27:41 -0400

    Removed: age from sequence

--- Spring_Boot_Learning/src/main/java/com/example/spring_boot_learning/student/Student.java
+++ Spring_Boot_Learning/src/main/java/com/example/spring_boot_learning/student/Student.java
@@ -3,9 +3,11 @@ package com.example.spring_boot_learning.student;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 
-@Entity
+
 @Table
+@Entity
 public class Student {
     @Id
     @SequenceGenerator(
@@ -23,7 +25,8 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
-    private int age;
+    @Transient
+    private Integer age;
 
     public Student() {
 
@@ -32,23 +35,20 @@ public class Student {
     public Student(Long id,
                    String name,
                    String email,
-                   LocalDate dob,
-                   int age) {
+                   LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Student(String name,
                    String email,
-                   LocalDate dob,
-                   int age) {
+                   LocalDate dob
+    ) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -84,7 +84,7 @@ public class Student {
     }
 
     public int getAge() {
-        return age;
+        return Period.between(dob, LocalDate.now()).getYears();
     }
 
     public void setAge(int age) {
