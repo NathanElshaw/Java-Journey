f29ad4a9f9c874c3fe1902647abd871995c90f36
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-24 20:49:25 -0400

    TInkering with security

--- Springboot_Login/src/main/java/com/example/springboot_login/Security/Config/WebSecurityConfig.java
+++ Springboot_Login/src/main/java/com/example/springboot_login/Security/Config/WebSecurityConfig.java
@@ -1,49 +1,28 @@
 package com.example.springboot_login.Security.Config;
 
-import com.example.springboot_login.AppUser.AppUserService;
-import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+        import lombok.AllArgsConstructor;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+        import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+        import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private final AppUserService appUserService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
     @Bean
-    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/registration/**").permitAll()
+                        .requestMatchers("/", "/api/v*/registration/**").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults())
-                .formLogin(login -> login.loginPage("/"))
+                .formLogin(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
-    }
-
-
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider =
-                new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(bCryptPasswordEncoder);
-        provider.setUserDetailsService(appUserService);
-        return provider;
     }
 }
