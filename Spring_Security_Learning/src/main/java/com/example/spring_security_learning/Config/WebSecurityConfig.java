df50681b12faf3a50af3c879f64dcb6d67dff758
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-26 21:58:00 -0400

    Added: security learning work

---
+++ Spring_Security_Learning/src/main/java/com/example/spring_security_learning/Config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.spring_security_learning.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/greet").permitAll();//Open apis endpoints
+                    auth.anyRequest().authenticated();//Secure all end points other than ones marked above.
+                })
+                 .formLogin(withDefaults())
+                 .build();
+    }
+}
---
+++ Spring_Security_Learning/src/main/java/com/example/spring_security_learning/Controller/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_learning.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greet")
+    public String Hello() {
+        return "Hello";
+        }
+}
