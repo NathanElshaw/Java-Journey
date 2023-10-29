9b0b1062f032395ae5999d4cd4729a7db73e37c1
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-28 23:18:30 -0400

    Added: handling api post

--- Springboot_Login/src/main/java/com/example/springboot_login/Registration/RegistrationService.java
+++ Springboot_Login/src/main/java/com/example/springboot_login/Registration/RegistrationService.java
@@ -1,12 +1,32 @@
 package com.example.springboot_login.Registration;
 
+import com.example.springboot_login.AppUser.AppUser;
+import com.example.springboot_login.AppUser.AppUserRole;
+import com.example.springboot_login.AppUser.AppUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class RegistrationService {
-    public static String register() {
-        return "works";
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+    public String register(RegistrationRequest request) {
+        boolean isValid = emailValidator
+                .test(request.getEmail());
+
+        if(!isValid || request.getEmail() == null){
+            throw new IllegalStateException("Email is not Valid");
+        }
+        return appUserService.singUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.User,
+                false,
+                false
+        ));
     }
 }
