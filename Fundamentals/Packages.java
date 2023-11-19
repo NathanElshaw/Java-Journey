71c20f85d7675befb1d65ccec42caf6efa06d8d2
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-18 23:26:54 -0500

    Package notes

---
+++ Fundamentals/Packages.java
@@ -0,0 +1,23 @@
+package Fundamentals;
+
+public class Packages {
+}
+
+/*
+A java package is a group of similar types of classes, interfaces, and sub-packages
+
+Java has some has built-in packages like java, lang, awt, javax, swing, net, io, util, sql etc.
+
+To access packages from another package you using (package package.name for classes or package package)
+
+Import process goes as
+Package -> Import -> Class
+
+
+SubPackages
+
+subpackages are packages within another package
+
+
+
+ */
